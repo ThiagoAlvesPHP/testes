e89d535e41e718d76bd8397901bdedd276344737
ThiagoAlvesPHP <thiagoalves@ltdeveloper.com.br>
2024-12-20 08:32:37 -0300

    feature: githooks

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -4,7 +4,13 @@
 current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 # Check for restricted branches
+#if [ "$current_branch" = "production" ] || [ "$current_branch" = "stage" ] || [ "$current_branch" = "homolog" ]; then
+#    echo "ERRO: Você está na branch $current_branch. Commits diretos nessa branch não são permitidos!"
+#    exit 1
+#fi
+
 if [ "$current_branch" = "production" ] || [ "$current_branch" = "stage" ] || [ "$current_branch" = "homolog" ]; then
-    echo "ERRO: Você está na branch $current_branch. Commits diretos nessa branch não são permitidos!"
-    exit 1
+  echo "ERRO: Você está na branch $current_branch. Commits diretos nessa branch não são permitidos!"
+  git push --no-verify origin refs/heads/$current_branch:$current_branch
+  exit 1
 fi
